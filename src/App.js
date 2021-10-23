bc9e9c84eb61e1842c13718229b937dcafb022d2
emiz98 <64089619+emiz98@users.noreply.github.com>
2021-10-24 04:49:46 +0530

    starter commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <img
+            src="http://localhost:3000/logo.svg"
+            className="App-logo"
+            alt="logo"
+          />
+        </div>
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
